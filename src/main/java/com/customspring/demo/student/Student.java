68aba6aeb1524638dd8b11e474afc41e95faaeb8
tondra93 <tondranahian@gmail.com>
2024-11-19 03:25:42 +0600

    database connected

--- src/main/java/com/customspring/demo/student/Student.java
+++ src/main/java/com/customspring/demo/student/Student.java
@@ -1,7 +1,20 @@
 package com.customspring.demo.student;
-import java.time.LocalDate;
+import jakarta.persistence.*;
 
+import java.time.LocalDate;
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private Integer age;
